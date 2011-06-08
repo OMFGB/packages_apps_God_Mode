bfe29fae950558df001aa2da898e28ebae8c77d9
Jonas Larsson <jonas@hallerud.se>
2011-06-09 01:30:56 +0200

    Configurable 90, 180 and 270 degree rotation Change-Id: I93481f75feb2b531cc97296f096203c6213422af Requires change to frameworks/base

--- src/com/t3hh4xx0r/god_mode/UiOptions.java
+++ src/com/t3hh4xx0r/god_mode/UiOptions.java
@@ -35,6 +35,15 @@ public class UiOptions extends PreferenceActivity implements OnPreferenceChangeL
         private static final String UI_EXP_WIDGET_PICKER = "widget_picker";
         private static final String OVERSCROLL_PREF = "pref_overscroll_effect";
         private static final String OVERSCROLL_WEIGHT_PREF = "pref_overscroll_weight";	
+        
+        // Rotation preferences
+        private static final String ROTATION_90_PREF = "pref_rotation_90";
+		private static final String ROTATION_180_PREF = "pref_rotation_180";
+		private static final String ROTATION_270_PREF = "pref_rotation_270";
+		
+		private CheckBoxPreference mRotation90Pref;
+		private CheckBoxPreference mRotation180Pref;
+		private CheckBoxPreference mRotation270Pref;
 
 	private CheckBoxPreference mUseScreenOnAnim;
 	private CheckBoxPreference mUseScreenOffAnim;
@@ -82,6 +91,20 @@ public class UiOptions extends PreferenceActivity implements OnPreferenceChangeL
 	        int overscrollWeight = Settings.System.getInt(getContentResolver(), Settings.System.OVERSCROLL_WEIGHT, 5);
         	mOverscrollWeightPref.setValue(String.valueOf(overscrollWeight));
 	        mOverscrollWeightPref.setOnPreferenceChangeListener(this);
+	        
+	        
+	        
+	        // Rotation
+	        /* Rotation */
+	        mRotation90Pref = (CheckBoxPreference) prefSet.findPreference(ROTATION_90_PREF);
+			mRotation180Pref = (CheckBoxPreference) prefSet.findPreference(ROTATION_180_PREF);
+			mRotation270Pref = (CheckBoxPreference) prefSet.findPreference(ROTATION_270_PREF);
+			int mode = Settings.System.getInt(getContentResolver(),
+			Settings.System.ACCELEROMETER_ROTATION_MODE, 5);
+			mRotation90Pref.setChecked((mode & 1) != 0);
+			mRotation180Pref.setChecked((mode & 2) != 0);
+			mRotation270Pref.setChecked((mode & 4) != 0);
+	        
 
     }
 
@@ -116,6 +139,14 @@ public class UiOptions extends PreferenceActivity implements OnPreferenceChangeL
                     readWidgetColor());
             cp.show();
 	}
+        if (preference == mRotation90Pref || preference == mRotation180Pref || preference == mRotation270Pref) {
+        	int mode = 0;
+				if (mRotation90Pref.isChecked())  mode |= 1;
+				if (mRotation180Pref.isChecked()) mode |= 2;
+				if (mRotation270Pref.isChecked()) mode |= 4;
+				Settings.System.putInt(getContentResolver(),
+				Settings.System.ACCELEROMETER_ROTATION_MODE, mode);
+        }
 
         return true;
     }
