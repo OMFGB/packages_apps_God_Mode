a75b867bce2d4e4bd7bcd0fb538789dca64dadf1
Linuxmotion <linuxmotion@thelinuxmotion.org>
2011-06-29 21:16:26 -0700

    Update package for lockscreen addition to set landscape orientation. Cleanup code.

--- src/com/t3hh4xx0r/god_mode/LockscreenMode.java
+++ src/com/t3hh4xx0r/god_mode/LockscreenMode.java
@@ -30,27 +30,23 @@ implements SharedPreferences.OnSharedPreferenceChangeListener {
 	private static final String VOLUME_WAKE_SCREEN = "volume_wake_screen";
 	private static final String TRACKPAD_UNLOCK_SCREEN = "trackpad_unlock_screen";
 	private static final String MENU_UNLOCK_SCREEN = "menu_unlock_screen";
-	private static final String LOCKSCREEN_ROTARY_LOCK = "use_rotary_lockscreen";
 	private static final String LOCKSCREEN_SHORTCUTS = "lockscreen_shortcuts";
 	private static final String LOCKSCREEN_ALWAYS_BATTERY = "lockscreen_always_battery";
+	private static final String LOCKSCREEN_TYPE = "lockscreen_type";
+	private static final String LOCKSCREEN_ORIENTATION = "lockscreen_orientation";
 	
+	private ListPreference mLockScreenTypeList;
 	private EditTextPreference mCarrierCaption;
 	private CheckBoxPreference mTrackpadWakeScreen;
 	private CheckBoxPreference mVolumeWakeScreen;
 	private CheckBoxPreference mTrackpadUnlockScreen;
 	private CheckBoxPreference mMenuUnlockScreen;
 	private CheckBoxPreference mLockscreenShortcuts;
-	//TODO Create music controls in rotary layouts.
-	//private CheckBoxPreference mUseRotaryLockPref;
-
-	private String HONEYCOMB = "HoneyComb";
-	private String TAB = "Tab";
-	private String ROTARY = "Rotary";
-	private String LOCKSCREENTYPE = "lockscreen_type";
-	
-	private ListPreference mLockScreenTypeList;
-	
+	private CheckBoxPreference mLockscreenOrientation;
 	private CheckBoxPreference mLockscreenAlwaysBattery;
+
+	
+
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {	
@@ -58,26 +54,33 @@ implements SharedPreferences.OnSharedPreferenceChangeListener {
 		addPreferencesFromResource(R.xml.lockscreen_prefs);
 		PreferenceScreen prefSet = getPreferenceScreen();
 		
+		// Carrier caption preference
 		mCarrierCaption = (EditTextPreference)prefSet.findPreference(CARRIER_CAP);
+		// Track pad wake preference
 		mTrackpadWakeScreen = (CheckBoxPreference) prefSet.findPreference(TRACKPAD_WAKE_SCREEN);
 		mTrackpadWakeScreen.setChecked(Settings.System.getInt(getContentResolver(), Settings.System.TRACKPAD_WAKE_SCREEN, 0) != 0);
+		// Volume wake preference
 		mVolumeWakeScreen = (CheckBoxPreference) prefSet.findPreference(VOLUME_WAKE_SCREEN);
 		mVolumeWakeScreen.setChecked(Settings.System.getInt(getContentResolver(), Settings.System.VOLUME_WAKE_SCREEN, 0) != 0);
+		// Track Pad unlock preference
 		mTrackpadUnlockScreen = (CheckBoxPreference) prefSet.findPreference(TRACKPAD_UNLOCK_SCREEN);
 		mTrackpadUnlockScreen.setChecked(Settings.System.getInt(getContentResolver(), Settings.System.TRACKPAD_UNLOCK_SCREEN, 0) != 0);
+		// Menu unlock preference
 		mMenuUnlockScreen = (CheckBoxPreference) prefSet.findPreference(MENU_UNLOCK_SCREEN);
 		mMenuUnlockScreen.setChecked(Settings.System.getInt(getContentResolver(), Settings.System.MENU_UNLOCK_SCREEN, 0) != 0);
-	        //TODO Create music controls in rotary layouts.
-		//mUseRotaryLockPref = (CheckBoxPreference)prefSet.findPreference(LOCKSCREEN_ROTARY_LOCK);
-		//mUseRotaryLockPref.setChecked(Settings.System.getInt(getContentResolver(), Settings.System.USE_ROTARY_LOCKSCREEN, 0) != 0);
+		// Shortcuts preference
 		mLockscreenShortcuts = (CheckBoxPreference) prefSet.findPreference(LOCKSCREEN_SHORTCUTS);
 		mLockscreenShortcuts.setChecked (Settings.System.getInt(getContentResolver(), Settings.System.LOCKSCREEN_SHORTCUTS, 0) == 0);
+		// Battery preference
 		mLockscreenAlwaysBattery = (CheckBoxPreference) prefSet.findPreference(LOCKSCREEN_ALWAYS_BATTERY);
 		mLockscreenAlwaysBattery.setChecked(Settings.System.getInt(getContentResolver(), Settings.System.LOCKSCREEN_ALWAYS_BATTERY, 0) == 0);
-		
-		
-		mLockScreenTypeList  = (ListPreference) findPreference("lockscreen_type");
+		// Lockscreen type preference
+		mLockScreenTypeList  = (ListPreference) findPreference(LOCKSCREEN_TYPE);
 		mLockScreenTypeList.setValueIndex(Settings.System.getInt(getContentResolver(), Settings.System.LOCKSCREEN_TYPE, 1)-1);
+		// Lockscreen oreientation preference
+		mLockscreenOrientation = (CheckBoxPreference) this.findPreference(LOCKSCREEN_ORIENTATION);
+		//default to portrait
+		mLockscreenOrientation.setChecked(Settings.System.getInt(getContentResolver(), Settings.System.LOCKSCREEN_ORIENTATION, 0) == 0);
 		
 		
 		getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
@@ -89,37 +92,33 @@ implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
         boolean value;
-        //TODO Create music controls in rotary layouts.
-        //if (preference == mUseRotaryLockPref) {
-        //    value = mUseRotaryLockPref.isChecked();
-        //    Settings.System.putInt(getContentResolver(), Settings.System.USE_ROTARY_LOCKSCREEN, value ? 1 : 0);
-        //    //Temporary hack to fix Phone FC's when swapping styles.
-        //   ActivityManager am = (ActivityManager)getSystemService(
-        //            Context.ACTIVITY_SERVICE);
-        //   am.forceStopPackage("com.android.phone");
-        //} else 
-	if (preference == mLockscreenAlwaysBattery) {
-		    value = mLockscreenAlwaysBattery.isChecked();
-		    Settings.System.putInt(getContentResolver(), Settings.System.LOCKSCREEN_ALWAYS_BATTERY, value ? 1 : 0);
-	} else if (preference == mLockscreenShortcuts) {
-		    value = mLockscreenShortcuts.isChecked();
-		    Settings.System.putInt(getContentResolver(), Settings.System.LOCKSCREEN_SHORTCUTS, value ? 1 : 0);
-	} else if (preference == mTrackpadWakeScreen) {
-		    value = mTrackpadWakeScreen.isChecked();
-		    Settings.System.putInt(getContentResolver(), Settings.System.TRACKPAD_WAKE_SCREEN, value ? 1 : 0);
-	} else if (preference == mVolumeWakeScreen) {
-		    value = mVolumeWakeScreen.isChecked();
-		    Settings.System.putInt(getContentResolver(), Settings.System.VOLUME_WAKE_SCREEN, value ? 1 : 0);
-	} else if (preference == mTrackpadUnlockScreen) {
-		    value = mTrackpadUnlockScreen.isChecked();
-		    Settings.System.putInt(getContentResolver(), Settings.System.TRACKPAD_UNLOCK_SCREEN, value ? 1 : 0);
-	} else if (preference == mMenuUnlockScreen) {
-		    value = mMenuUnlockScreen.isChecked();
-		    Settings.System.putInt(getContentResolver(), Settings.System.MENU_UNLOCK_SCREEN, value ? 1 : 0);
-	}else if ( preference == mLockScreenTypeList){
-		
-		
-	}
+        
+		if (preference == mLockscreenAlwaysBattery) {
+			    value = mLockscreenAlwaysBattery.isChecked();
+			    Settings.System.putInt(getContentResolver(), Settings.System.LOCKSCREEN_ALWAYS_BATTERY, value ? 1 : 0);
+		} else if (preference == mLockscreenShortcuts) {
+			    value = mLockscreenShortcuts.isChecked();
+			    Settings.System.putInt(getContentResolver(), Settings.System.LOCKSCREEN_SHORTCUTS, value ? 1 : 0);
+		} else if (preference == mTrackpadWakeScreen) {
+			    value = mTrackpadWakeScreen.isChecked();
+			    Settings.System.putInt(getContentResolver(), Settings.System.TRACKPAD_WAKE_SCREEN, value ? 1 : 0);
+		} else if (preference == mVolumeWakeScreen) {
+			    value = mVolumeWakeScreen.isChecked();
+			    Settings.System.putInt(getContentResolver(), Settings.System.VOLUME_WAKE_SCREEN, value ? 1 : 0);
+		} else if (preference == mTrackpadUnlockScreen) {
+			    value = mTrackpadUnlockScreen.isChecked();
+			    Settings.System.putInt(getContentResolver(), Settings.System.TRACKPAD_UNLOCK_SCREEN, value ? 1 : 0);
+		} else if (preference == mMenuUnlockScreen) {
+			    value = mMenuUnlockScreen.isChecked();
+			    Settings.System.putInt(getContentResolver(), Settings.System.MENU_UNLOCK_SCREEN, value ? 1 : 0);
+		}else if ( preference == mLockScreenTypeList){
+			// Do Nothing
+			
+		}else if ( preference ==  mLockscreenOrientation){
+			    value = mLockscreenOrientation.isChecked();
+			    Settings.System.putInt(getContentResolver(), Settings.System.LOCKSCREEN_ORIENTATION, value ? 1 : 0);
+			
+		}
         return true;
     }
 	
