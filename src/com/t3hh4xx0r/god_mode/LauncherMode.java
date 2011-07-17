ede50b738d0dcfceb71558b6c91ea171a27124b6
r2DoesInc <r2doesinc@gmail.com>
2011-07-17 19:54:14 -0400

    Add four or two hotseat option to god mode.

--- src/com/t3hh4xx0r/god_mode/LauncherMode.java
+++ src/com/t3hh4xx0r/god_mode/LauncherMode.java
@@ -50,6 +50,7 @@ implements SharedPreferences.OnSharedPreferenceChangeListener {
 	CheckBoxPreference mScreenCheckBox;
 	CheckBoxPreference mLauncherEndlessLoop;
 	CheckBoxPreference mWallpaperLoop;
+        CheckBoxPreference mFourHotseats;
 
     private ActivityManager activityManager;
 	
@@ -61,10 +62,9 @@ implements SharedPreferences.OnSharedPreferenceChangeListener {
     private static final String SCREENSETTINGS = "NUM_SCREENS";
     private static final String LAUNCHER_ENDLESS_LOOP = "launcher_endless_loop";
     private static final String WALLPAPER_LOOP = "wallpaper_loop";
+    private static final String FOUR_HOTSEATS = "four_hotseats"; 
 
     private static final String LAUNCHER = "com.android.launcher";
-    
-  
 
 	@Override
     public void onCreate(Bundle savedInstanceState) {	
@@ -83,15 +83,19 @@ implements SharedPreferences.OnSharedPreferenceChangeListener {
 	PreferenceScreen prefSet = getPreferenceScreen();
 	mScreenCheckBox = (CheckBoxPreference) findPreference("screen_changer");
 	mScreenCheckBox.setChecked(mIsScreenChangerOn);
+
 	mWallpaperLoop = (CheckBoxPreference) prefSet.findPreference(WALLPAPER_LOOP);
 	mWallpaperLoop.setChecked(Settings.System.getInt(getContentResolver(),
 			Settings.System.WALLPAPER_LOOP, 1) == 1);
+
 	mLauncherEndlessLoop = (CheckBoxPreference) prefSet.findPreference(LAUNCHER_ENDLESS_LOOP);
 	mLauncherEndlessLoop.setChecked(Settings.System.getInt(getContentResolver(),
 			Settings.System.LAUNCHER_ENDLESS_LOOP, 1) == 1);
-        mWallpaperLoop = (CheckBoxPreference) prefSet.findPreference(WALLPAPER_LOOP);
-        mWallpaperLoop.setChecked(Settings.System.getInt(getContentResolver(),
-                        Settings.System.WALLPAPER_LOOP, 1) == 1);
+
+	mFourHotseats = (CheckBoxPreference) prefSet.findPreference(FOUR_HOTSEATS);
+        mFourHotseats.setChecked(Settings.System.getInt(getContentResolver(),
+			Settings.System.FOUR_HOTSEATS, 1) == 1);
+
 	mScreenPreference = (ListPreference) findPreference("num_screens");
 	
 	activityManager = (ActivityManager)this.getSystemService(ACTIVITY_SERVICE);
@@ -108,6 +112,12 @@ implements SharedPreferences.OnSharedPreferenceChangeListener {
 		restartLauncher2(activityManager);
 	    }
 
+	    if (preference == mFourHotseats) {
+		value = mFourHotseats.isChecked();
+		Settings.System.putInt(getContentResolver(),
+			Settings.System.FOUR_HOTSEATS, value ? 1 : 0);
+	    }
+
 	    if (preference == mLauncherEndlessLoop) {
 		value = mLauncherEndlessLoop.isChecked();
 		Settings.System.putInt(getContentResolver(),
@@ -116,7 +126,7 @@ implements SharedPreferences.OnSharedPreferenceChangeListener {
 	    }
 	    
 	    if (preference == mScreenCheckBox && mIsScreenChangerOn == false) {
-			// Ask the user if they are sure they whant to proceed/
+		// Ask the user if they are sure they whant to proceed/
 	    	// If they do let them know that the homescreen widgets/apps 
 	    	// will be reset
 	    	alertbox("Warning", "Selecting a new homescreen configuration will remove the current configuration. " +
