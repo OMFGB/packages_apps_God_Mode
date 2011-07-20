de85b9b3cc41252b1c2e0d5f2a3cc6d886bd0476
LinuxMotion <jweyrauch359@gmail.com>
2011-07-19 17:03:29 -0700

    Add a basic generic JSONParsing class and interface. Used it the nightlies and addons classes to download, parse, and help to send the correct UI creation signals based upon defined error functions.

---
+++ src/com/t3hh4xx0r/addons/web/JSON/JSONUtils.java
@@ -0,0 +1,175 @@
+package com.t3hh4xx0r.addons.web.JSON;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
+import android.util.Log;
+
+import com.android.internal.widget.CircularSelector.OnCircularSelectorTriggerListener;
+import com.t3hh4xx0r.addons.AddonsObject;
+import com.t3hh4xx0r.addons.OMFGBExternalAddonsAppAddonsActivity;
+import com.t3hh4xx0r.addons.OnAddonsPreferenceClickListener;
+import com.t3hh4xx0r.addons.utils.Constants;
+import com.t3hh4xx0r.addons.utils.DeviceType;
+import com.t3hh4xx0r.addons.utils.DownloadFile;
+
+public class JSONUtils {
+	
+	JSONParsingInterface mJSONParsingInterface;
+	Context mContext;
+	
+	private String TAG = "JSONParsingInterface";
+	
+	public PreferenceScreen ParseJSON(PreferenceActivity activity, Context context){
+		
+		 mContext = context;
+		 PreferenceScreen PreferenceRoot = activity.getPreferenceManager().createPreferenceScreen(mContext);
+		 InputStream is = downloadJSONScript();
+		 
+		 if(is != null){
+			 
+		
+			 try {
+				 
+				 PreferenceRoot = mJSONParsingInterface.ParseJSONScript(PreferenceRoot,is);
+				
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				PreferenceRoot = mJSONParsingInterface.unableToParseScript();
+			}
+		 
+		 }
+		 else{
+			 PreferenceRoot = mJSONParsingInterface.unableToDownloadScript();
+			 
+		 }
+		return PreferenceRoot;
+	}
+	
+	/**
+	 * Downloads the JSON script.
+	 * Performs a check to make sure that the 
+	 * file directory is present
+	 * 
+	 * @return is the inputstream for the json script
+	 */
+	private InputStream downloadJSONScript(){
+		
+		
+
+    		
+            InputStream is = null;
+            Log.d(TAG, "Begining json download");
+            
+            if(checkDownloadDirectory()){
+            	
+            	File updateFile = new File(Constants.DOWNLOAD_DIR + Constants.getDeviceScript());
+	           	try{
+	           		Log.i(TAG, "The update path and file is called: " + updateFile.toString());
+	           		// Needed because the manager does not handle https connections
+	           		DownloadFile.updateAppManifest(Constants.getDeviceScript());
+	           		
+	           		is = new FileInputStream(updateFile);
+	           	
+	           	}
+	           	catch(FileNotFoundException e){
+	           		
+	           			e.printStackTrace();
+	           			if(true)Log.d(TAG, "Could not update app from file resource," +
+	           					" the file was not found. Reverting to nothing");
+	           			is = null;
+	                   	
+	           	}
+	           	
+            	
+            }// End checkdownloaddir if statement
+           
+           
+            
+
+            // If this point is reached then the input stream is null  
+        return is;
+        
+        }
+	
+	/**
+	 * Checks to see if the download directory
+	 * for t3hh4xx0r is created. If is
+	 * not created, it will create it.
+	 * 
+	 * 
+	 * @return true if the directory exists or if the directory was created if needed, false otherwise.
+	 */
+	private Boolean checkDownloadDirectory(){
+		
+		File f = new File(Constants.DOWNLOAD_DIR );
+		boolean success;
+		if(!f.exists()){
+			
+			Log.i(TAG, "File diretory does not exist, creating it");
+			success = f.mkdirs();
+			if(!success)Log.d(TAG, "Directory creation failed");
+			
+			
+		}
+		else {
+			f = null;
+			success = true;
+
+			Log.i(TAG, "File directory  exist.");
+			
+		}
+		return success;
+		
+	}
+		
+	
+	
+	
+	public interface JSONParsingInterface {
+		
+	
+	/**
+     * Create three categories for the addons preferncescreen then adds them.
+     * The three categories will populate the list dynamicly from
+     * a parsed JSON file.
+     * 
+     * If a category is empty it will be reomved before displaying
+     * 
+     * @return PreferenceScreen the screen preference that will be insflated
+     */
+	 PreferenceScreen ParseJSONScript(PreferenceScreen PreferenceRoot, InputStream is) throws JSONException;
+	 PreferenceScreen unableToParseScript();
+	 PreferenceScreen unableToDownloadScript();
+    
+    
+	}
+	
+	
+	/**
+     * Registers a callback to be invoked when the music controls
+     * are "triggered" by sliding the view one way or the other
+     * or pressing the music control buttons.
+     *
+     * @param l the OnMusicTriggerListener to attach to this view
+     */
+    public void setJSONUtilsParsingInterface(JSONParsingInterface l) {
+    	
+    	this.mJSONParsingInterface = l;
+    }
+	
+	
+	
+	
+}
