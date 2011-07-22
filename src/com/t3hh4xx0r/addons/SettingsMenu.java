4eb920c9593386f823d312e39c06e29d62c95c93
LinuxMotion <jweyrauch359@gmail.com>
2011-07-22 14:15:09 -0700

    Add a main settings menu

---
+++ src/com/t3hh4xx0r/addons/SettingsMenu.java
@@ -0,0 +1,16 @@
+package com.t3hh4xx0r.addons;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class SettingsMenu extends PreferenceActivity {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		
+		
+		
+		
+	}
+
+}
