d444a86fcbc949d55ef9ba5d24e842dfe0f73d25
r2doesinc <r2doesinc@gmail.com>
2011-08-16 19:19:26 -0400

    MD5 values are returning the wrong value :(

--- src/com/t3hh4xx0r/addons/utils/AlertsBroadcastReceivers.java
+++ src/com/t3hh4xx0r/addons/utils/AlertsBroadcastReceivers.java
@@ -1,5 +1,6 @@
 package com.t3hh4xx0r.addons.utils;
 
+import com.t3hh4xx0r.R;
 import com.t3hh4xx0r.addons.alerts.OMFGBAlertsActivity;
 
 import java.io.BufferedInputStream;
@@ -17,6 +18,9 @@ import java.security.*;
 import java.math.*;
 
 import android.app.DownloadManager;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -29,7 +33,8 @@ import android.util.Slog;
 public class AlertsBroadcastReceivers extends BroadcastReceiver {
 	public static String TAG = "Receivers";
 	private static String updateDirectory = "/sdcard/t3hh4xx0r/updates/";
-	private String OUTPUT;
+
+        Constants.LOCAL_FILE = "/sdcard/t3hh4xx0r/downloads/" + Constants.ALERTS;
 
 	Context mContext;
 	DownloadManager downloadManager;
@@ -37,18 +42,17 @@ public class AlertsBroadcastReceivers extends BroadcastReceiver {
 	public void onReceive(Context context, Intent intent) {
    	   Slog.d(TAG, "Received");
 	   //First lets start with the bulletin board posts
-	   downloadAlerts();
+	   downloadAlerts(context);
 	}
 
-	private void downloadAlerts() {
-           Constants.LOCAL_FILE = "/sdcard/t3hh4xx0r/downloads/" + Constants.ALERTS;
+	private void downloadAlerts(Context context) {
            File local = new File(Constants.LOCAL_FILE);
            if (!local.exists()) {
 		refreshLocal();
                 Slog.d(TAG, "Local file refreshed, skipping update check.");
            } else {
                 getAlertsValues();
-     	        diffChecks();
+     	        diffChecks(context);
 	   }
 	}
 
@@ -57,49 +61,64 @@ public class AlertsBroadcastReceivers extends BroadcastReceiver {
    }
 
    public void getAlertsValues() {
-	Slog.d(TAG, "local file is " + Constants.LOCAL_FILE); 
-	checkMd5(Constants.LOCAL_FILE);
-	Constants.LOCAL_MD5 = Constants.OUTPUT_MD5;
-	Slog.d(TAG, "Local md5: " + Constants.LOCAL_MD5);
-	Constants.OUTPUT_MD5 = null;
+        Constants.LOCAL_MD5 = checkMd5(Constants.LOCAL_FILE);
+	Slog.d(TAG, "Local md5: " + checkMd5(Constants.LOCAL_FILE));
 
 	Downloads.refreshAlerts();
-	checkMd5(Constants.LOCAL_FILE);
-	Constants.REMOTE_MD5 = Constants.OUTPUT_MD5;
-	Slog.d(TAG, "remote md5: " + Constants.REMOTE_MD5);
- 	Constants.OUTPUT_MD5 = null;
+        Constants.REMOTE_MD5 = checkMd5(Constants.LOCAL_FILE);
+	Slog.d(TAG, "remote md5: " + checkMd5(Constants.LOCAL_FILE));
     }
 
-    public void diffChecks() {
+    public void diffChecks(Context context) {
 	Slog.d(TAG, "BEGINNING DIFFCHECKS");
-	if (Constants.LOCAL_MD5 != Constants.REMOTE_MD5) {
-	    alertUser();
+	if (!Constants.LOCAL_MD5.equals(Constants.REMOTE_MD5)) {
+	    alertUser(context);
 	} else {
 	    Slog.d(TAG, "Your Alerts are up to date.");
 	}
+        Constants.REMOTE_MD5 = null;
+        Constants.LOCAL_MD5 = null;
     }
 
-    public void alertUser() {
+    public void alertUser(Context context) {
         Slog.d(TAG, "You have new alerts.");
-        Slog.d(TAG, Constants.LOCAL_MD5 + " != " + Constants.REMOTE_MD5);
+
+		 String ns = Context.NOTIFICATION_SERVICE;
+		 NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(ns);
+
+		 int icon = R.drawable.icon;        // icon from resources
+		 CharSequence tickerText = "T3hh4xx0r";              // ticker-text
+		 long when = System.currentTimeMillis();         // notification time
+		 CharSequence contentTitle = "T3hh4xx0r Addons";  // expanded message title
+		 CharSequence contentText = "Download completed";      // expanded message text
+
+		 Intent notificationIntent = new Intent(context, AlertsBroadcastReceivers.class);
+
+		 PendingIntent contentIntent = PendingIntent.getActivity(context, 0, notificationIntent, 0);
+
+		 // the next two lines initialize the Notification, using the configurations above
+		 Notification notification = new Notification(icon, tickerText, when);
+		 notification.setLatestEventInfo(context, contentTitle, contentText, contentIntent);
+		 final int HELLO_ID = 1;
+
+		 mNotificationManager.notify(HELLO_ID, notification);
     }
 
-    public static String checkMd5(String string) {
-    	    byte[] hash;
-
-	    try {
-	        hash = MessageDigest.getInstance("MD5").digest(string.getBytes("UTF-8"));
-	    } catch (NoSuchAlgorithmException e) {
-	        throw new RuntimeException("Huh, MD5 should be supported?", e);
-	    } catch (UnsupportedEncodingException e) {
-        	throw new RuntimeException("Huh, UTF-8 should be supported?", e);	    
-	    }	
-	    StringBuilder hex = new StringBuilder(hash.length * 2);
-	    for (byte b : hash) {
-	        if ((b & 0xFF) < 0x10) hex.append("0");
-	        hex.append(Integer.toHexString(b & 0xFF));
-	    }
-            Constants.OUTPUT_MD5 = hex.toString();
-	    return hex.toString();
-      }
+    public String checkMd5(String s) {
+    try {
+        // Create MD5 Hash
+        MessageDigest digest = java.security.MessageDigest.getInstance("MD5");
+        digest.update(s.getBytes());
+        byte messageDigest[] = digest.digest();
+        //Create Hex String
+        StringBuffer hexString = new StringBuffer();
+        for (int i=0; i<messageDigest.length; i++)
+            hexString.append(Integer.toHexString(0xFF & messageDigest[i]));
+        return hexString.toString();
+        
+    } catch (NoSuchAlgorithmException e) {
+        e.printStackTrace();
+    }
+    return "";
+}
 }
